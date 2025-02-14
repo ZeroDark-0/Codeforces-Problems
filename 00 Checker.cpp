401183b79264c5325782cdb9c0d2bd6e0ed54450
ZeroDark <Chaitanyajain.official@gmail.com>
2025-02-14 22:09:44 +0530

    added file 2044A. Easy Problem.cpp, 2044B. Normal Problem.cpp, 2044C. Hard Problem.cpp, and Tester.cpp -- edited Checker.cpp

--- 00 Checker.cpp
+++ 00 Checker.cpp
@@ -1,7 +1,7 @@
 // experimental
 /*
  *2008B. Square or Not
- *
+ *2044C. Hard Problem
  *
  *
  *
---
+++ Tester.cpp
@@ -0,0 +1,14 @@
+// 1. 1A. Theater Square
+// #include <iostream>
+// #include <cmath>
+//
+// using namespace std;
+//
+// int main() {
+//     double n,m,a;
+//     cin >> n >> m >> a;
+//     long long ans = static_cast<long long>(ceil(n / a)) * static_cast<long long>(ceil(m / a));
+//     cout << ans;
+// }
+
+//---------------------------------------------------------
