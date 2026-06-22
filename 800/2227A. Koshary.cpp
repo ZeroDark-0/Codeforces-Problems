3dc6725e56dc442a884535a68d89f9eed8b575e7
ZeroDark <zerodark.hopium@gmail.com>
2026-06-22 16:11:28 +0800

    added files 2227A. Koshary.cpp

---
+++ 800/2227A. Koshary.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;cin>>t;
+    while (t-->0) {
+        int x,y;cin>>x>>y;
+        if (x%2!=0 && y%2!=0) {
+            cout <<"no"<<endl;
+        }
+        else {
+            cout <<"yes"<<endl;
+        }
+    }
+}
