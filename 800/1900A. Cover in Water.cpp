8206ad81f7be505ce05ec49914f8acf5b585ed8b
ZeroDark <zerodark.hopium@gmail.com>
2026-06-24 21:39:33 +0800

    added files 1900A. Cover in Water.cpp

---
+++ 800/1900A. Cover in Water.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t-- > 0) {
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+
+        if (s.find("...") != string::npos) {
+            cout << 2 << '\n';
+        } else {
+            cout << count(s.begin(), s.end(), '.') << '\n';
+        }
+    }
+
+}
--- Tester.cpp
+++ Tester.cpp
@@ -4,26 +4,5 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n;cin>>n;
-    vector<int> v(n);
-    for (int i = 0; i < n; i++) cin>>v[i];
 
-    if(is_sorted(v.begin(), v.end())) {
-        cout << "yes\n1 1\n";
-        return 0;
-    }
-    int l = 0;
-    while(l + 1 < n && v[l] <= v[l + 1])
-        l++;
-
-    int r = n - 1;
-    while(r - 1 >= 0 && v[r - 1] <= v[r])
-        r--;
-
-    reverse(v.begin() + l, v.begin() + r + 1);
-
-    if(is_sorted(v.begin(), v.end()))
-        cout << "yes\n" << l + 1 << " " << r + 1 << "\n";
-    else
-        cout << "no\n";
 }
